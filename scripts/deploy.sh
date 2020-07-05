8e31e0c87b286d31ef0bb1bb3c332da2e9f1eb00
Jordan Mance <mancej@snagajob.com>
2020-07-05 19:10:11 -0400

    Fixing deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 aws s3 sync site/ s3://figgy-website/docs/ --delete
-aws s3 sync site/images/ s3://figgy-website/images/--delete
+aws s3 sync site/images/ s3://figgy-website/images/ --delete
